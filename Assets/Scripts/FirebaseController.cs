226a30c6304f58e8236c85812a4be0225c9b69ea
openai-exe <sabeensaeed06@gmail.com>
2023-10-18 02:38:42 +0500

    Add Login, SignUp, Forget Password and Notification

---
+++ Assets/Scripts/FirebaseController.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FirebaseController : MonoBehaviour
+{
+    public GameObject loginPanel, signupPanel, profilePanel, forgetPasswordPanel, notificationPanel;
+    public InputField loginEmail, loginPassword, signupEmail, signupPassword, signupCPassword, signupUserName, forgetPassEmail;
+    public Text notif_Title_Text, notif_Message_Text, profileUserEmail_Text, profileUserName_Text;
+    public Toggle rememberMe;
+
+    public void OpenLoginPanel()
+    {
+        loginPanel.SetActive(true);
+        signupPanel.SetActive(false);
+        profilePanel.SetActive(false);
+        forgetPasswordPanel.SetActive(false);
+    }
+
+    public void OpenSignUpPanel()
+    {
+        loginPanel.SetActive(false);
+        signupPanel.SetActive(true);
+        profilePanel.SetActive(false);
+        forgetPasswordPanel.SetActive(false);
+    }
+
+    public void OpenProfilePanel()
+    {
+        loginPanel.SetActive(false);
+        signupPanel.SetActive(false);
+        profilePanel.SetActive(true);
+        forgetPasswordPanel.SetActive(false);
+    }
+
+    public void OpenForgetPassPanel()
+    {
+        loginPanel.SetActive(false);
+        signupPanel.SetActive(false);
+        profilePanel.SetActive(false);
+        forgetPasswordPanel.SetActive(true);
+    }
+
+    public void LoginUser()
+    {
+        if (string.IsNullOrEmpty(loginEmail.text) && string.IsNullOrEmpty(loginPassword.text))
+        {
+            ShowNotificationMessage("Error", "Fields Empty! Please input all details");
+            return; // Do Login
+        }
+        else
+        {
+            OpenProfilePanel();
+            return;
+        }
+    }
+
+    public void SignUpUser()
+    {
+        if (string.IsNullOrEmpty(signupEmail.text) && string.IsNullOrEmpty(signupPassword.text) && string.IsNullOrEmpty(signupCPassword.text) && string.IsNullOrEmpty(signupUserName.text))
+        {
+            ShowNotificationMessage("Error", "Fields Empty! Please input all details");
+            return; // Do Signup
+        }
+    }
+
+    public void ForgetPass()
+    {
+        if (string.IsNullOrEmpty(forgetPassEmail.text))
+        {
+            ShowNotificationMessage("Error", "Fields Empty! Please input all details");
+            return;
+        }
+    }
+
+    private void ShowNotificationMessage(string title, string message)
+    {
+        notif_Title_Text.text = title;
+        notif_Message_Text.text = message;
+
+        notificationPanel.SetActive(true);
+    }
+
+    public void CloseNotif_Panel()
+    {
+        notif_Title_Text.text = "";
+        notif_Message_Text.text = "";
+        notificationPanel.SetActive(false);
+    }
+
+    public void LogOut()
+    {
+        profilePanel.SetActive(false);
+        profileUserEmail_Text.text = "";
+        profileUserName_Text.text = "";
+        OpenLoginPanel();
+    }
+}
